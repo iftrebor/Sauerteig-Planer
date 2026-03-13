7d1e9fa28a35be89adf83fdd87bf4653a2d3675a
iftrebor <robert.plietzsch@icloud.com>
2026-03-13 13:51:59 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
-self.addEventListener('install', (e)=>{e.waitUntil(caches.open('lm-hefe-v1').then((c)=>c.addAll(['index.html','manifest.webmanifest','icons/bread-512.png','icons/bread-192.png','icons/bread-180.png','icons/bread-144.png','icons/bread-96.png','icons/bread-32.png']))});});self.addEventListener('fetch',(e)=>{e.respondWith(caches.match(e.request).then((r)=>r||fetch(e.request)));});
+
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open('lm-hefe-v4').then((cache)=>cache.addAll([
+    './',
+    './index.html',
+    './manifest.webmanifest',
+    './sw.js',
+    './icons/bread-512.png',
+    './icons/bread-192.png',
+    './icons/bread-180.png',
+    './icons/bread-144.png',
+    './icons/bread-96.png',
+    './icons/bread-32.png',
+    './icons/apple-touch-icon.png'
+  ])));
+});
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(caches.match(e.request).then((resp)=> resp || fetch(e.request)));
+});
